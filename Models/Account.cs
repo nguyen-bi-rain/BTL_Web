4b710b9c39083d8aaa30807f8b3aa4f8aed74629
nguyen-bi-rain <vunguyen16052003@gmail.com>
2023-10-23 13:51:02 +0700

    Chua tao account

--- Models/Account.cs
+++ Models/Account.cs
@@ -13,9 +13,7 @@ namespace ShopQuanAo.Models
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Email { get; set; }
-        public string? Phone { get; set; }
         public string? Password { get; set; }
-        public string? Fullname { get; set; }
         public int? Idrole { get; set; }
 
         public virtual Role? IdroleNavigation { get; set; }
