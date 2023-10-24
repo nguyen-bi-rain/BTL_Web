58a3c6cec550a573df124ca3f69e153572d9aaa9
ThienVuVan <>
2023-10-24 21:14:32 +0700

    commit

---
+++ Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using ShopQuanAo.Models;
+using System.Security.Principal;
+
+namespace ShopQuanAo.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly LTWEBContext db;
+        List<Product> cartItems;
+        public CartController(LTWEBContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+           
+            if (HttpContext.Session.Get<List<Product>>("Cart") == null)
+            {
+                cartItems = new List<Product>();
+                var product = db.Products.FirstOrDefault(p => p.Id == 1);
+                cartItems.Add(product);
+            }
+            else
+            {
+                cartItems = HttpContext.Session.Get<List<Product>>("Cart");
+            }
+
+            return View(cartItems);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int productId)
+        {
+            if (HttpContext.Session.Get<List<Product>>("Cart") == null)
+            {
+                cartItems = new List<Product>();
+            }
+            else
+            {
+                cartItems = HttpContext.Session.Get<List<Product>>("Cart");
+            }
+
+            // Thêm sản phẩm vào danh sách giỏ hàng
+            var product = db.Products.FirstOrDefault(p => p.Id == productId);
+            if (product != null)
+            {
+                cartItems.Add(product);
+            }
+
+            // Lưu danh sách sản phẩm vào session
+            HttpContext.Session.Set("Cart", cartItems);
+
+            return RedirectToAction("Index", "Cart");
+        }
+    }
+}
